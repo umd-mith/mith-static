ffef9c61392c53c0444b457297743924df03188b
kirstenkeister <kirsten.keister@gmail.com>
2020-09-30 19:21:06 -0400

    apply columns, add fields to People page

--- src/pages/people.js
+++ src/pages/people.js
@@ -4,27 +4,25 @@ import { graphql, Link } from 'gatsby';
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-const PeoplePage = ({ data }) => {
+const PeoplePage = ({ data }) => { 
   return (
 		<Layout>
       <div>
         <section>
-          <SEO title="MITH People" />
-          <h1>MITH People</h1>
+          <SEO title="People" />
+          <h1>People</h1>
         </section>
-        <section> 
-          {data.people.nodes.map(group => {
+        <section className="columns"> 
+          {data.people.nodes.map(person => {
+            const img = person.data.headshot 
+              ? <img src={person.data.headshot[0].thumbnails.large.url} alt={`Headshot of ${person.data.name}`}/>
+              : ''
             return (
-              <div key={`g-${group.data.id}`}>
-                <h3>{group.data.group_name}</h3>{
-                  group.data.linked_people ?
-                    group.data.linked_people.map(person => (
-                      <div>
-                        <Link key={`p-${person.data.id}`} to={person.data.slug}>{person.data.name}</Link>
-                      </div>
-                    ))
-                  : ''
-              }</div>
+              <article className="col-4 col-4-lg col-4-md col-6-sm col-6-xs" id={person.data.id} title={person.data.name} key={`p-${person.data.id}`}>
+                {img}
+                <h4><Link key={`p-${person.data.id}`} to={person.data.slug}>{person.data.name}</Link></h4>
+                <p>{person.data.title}</p>
+              </article>
             )
           })}
         </section>
@@ -35,18 +33,37 @@ const PeoplePage = ({ data }) => {
 
 export const query = graphql`
   query PeopleQuery {
-    people: allAirtable(filter: {table: {eq: "Staff Groups"}}, sort: {fields: data___sort}) {
+    people: allAirtable(
+      filter: {
+        table: {eq: "People"}, 
+        data: {group_type: {regex: "/^(?!P).*/"}}
+      }, 
+      sort: {
+        fields: data___last
+      }
+    ) {
       nodes {
         data {
           id
-          group_name
-          linked_people {
+          slug
+          name
+          first
+          last
+          title
+          staff_group {
             data {
-              id
-              name
-              slug
+              group_name
+              type
             }
           }
+          headshot {
+            thumbnails {
+              large {
+                url
+              }
+            }
+          }
+          group_type
         }
       }
     }
