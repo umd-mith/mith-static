5fcbd624d245a210aea003a8531a81e54554dc21
Trevor Muñoz <trevor@trevormunoz.com>
2020-10-11 19:47:18 -0400

    Remove restrictive copyright claim

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,7 +9,7 @@ class Footer extends React.Component {
       <footer>
         <div className="footer-bar">
           <div className="copyright">
-            Copyright &copy; 2015 &ndash; {new Date().getFullYear()} Maryland Institute for Technology in the Humanities &nbsp;|&nbsp; All Rights Reserved &nbsp;|&nbsp; <Link to="https://umd.edu/web-accessibility">Web Accessibility</Link> &nbsp;|&nbsp; <Link to="https://umd.edu/privacy-notice">Privacy Notice</Link>
+            Unless otherwise noted, this site and its contents are copyright &copy; 2005 &ndash; {new Date().getFullYear()} Maryland Institute for Technology in the Humanities and licensed under a <Link to="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</Link>. &nbsp;|&nbsp; <Link to="https://umd.edu/web-accessibility">Web Accessibility</Link> &nbsp;|&nbsp; <Link to="https://umd.edu/privacy-notice">Privacy Notice</Link>
           </div>
         </div>
       </footer>
