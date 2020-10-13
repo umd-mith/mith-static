4df5ddd6c247072c67c9389067f8b0292e82911d
Trevor Muñoz <trevor@trevormunoz.com>
2020-10-13 16:20:11 -0400

    Resolve React linter warnings in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,7 +12,7 @@ class Footer extends React.Component {
         <Nav className="container" />
         <div className="footer-bar container">
           <div className="copyright">
-            Unless otherwise noted, this site and its contents are copyright &copy; 2005 &ndash; {new Date().getFullYear()} Maryland Institute for Technology in the Humanities and licensed under a <Link to="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</Link>. &nbsp;|&nbsp; <Link to="https://umd.edu/web-accessibility">Web Accessibility</Link> &nbsp;|&nbsp; <Link to="https://umd.edu/privacy-notice">Privacy Notice</Link>
+            Unless otherwise noted, this site and its contents are copyright &copy; 2005 &ndash; {new Date().getFullYear()} Maryland Institute for Technology in the Humanities and licensed under a <a href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>. &nbsp;|&nbsp; <a href="https://umd.edu/web-accessibility">Web Accessibility</a> &nbsp;|&nbsp; <a href="https://umd.edu/privacy-notice">Privacy Notice</a>
           </div>
         </div>
       </footer>
