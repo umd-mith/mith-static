6ff4821db211eb01a62cd6566ea8166f707d163b
kirstenkeister <kirsten.keister@gmail.com>
2020-10-13 09:45:24 -0400

    fix second section column width on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => (
       <section className="leader">
         <p>MITH is a research center within the College of Arts and Humanities at the University of Maryland, College Park. The center is home to an interdisciplinary group of researchers who collaboratively advance the study of cultural heritage and arts using computational technologies and who also study computational technologies using humanities approaches. For over 20 years, this world-class concentration of expertise has served as a resource for the university community and collaborators in many domains. MITH also provides some teaching and hosts events for campus and public communities in support of its core research mission.</p>
       </section>
-      <section>The MITH website is being redeveloped in public. If you are looking for content that was previously here, it's in <a href="#">the archive</a>.</section>
+      <section className="col-12">The MITH website is being redeveloped in public. If you are looking for content that was previously here, it's in <a href="#">the archive</a>.</section>
     </div>
   </Layout>
 )
