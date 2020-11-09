85def5dda0e0c62ce786f35899f61336a2f4d2f9
Trevor Muñoz <trevor@trevormunoz.com>
2020-11-09 15:34:27 -0500

    Add the UMD brand header

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,5 +3,15 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
+import React from 'react';
 
-// You can delete this file if you're not using it
+const UMDBrandComponent = [
+    <script src="https://umd-header.umd.edu/build/bundle.js?search=0&search_domain=&events=0&news=0&schools=0&admissions=0&support=0&support_url=&wrapper=1100&sticky=0"></script>
+]
+
+export const onRenderBody = (
+    {setPostBodyComponents},
+    pluginOptions
+) => {
+    setPostBodyComponents(UMDBrandComponent);
+};
