7dd16423d219067e0ef9d5af9509d8a39ea6031d
kirstenkeister <kirsten.keister@gmail.com>
2020-12-02 12:25:35 -0500

    make paginator a section

--- src/components/paginator.js
+++ src/components/paginator.js
@@ -4,20 +4,20 @@ import React from 'react'
 import paginatorStyles from './paginator.module.css'
 
 const Paginator = ({count, path}) => {
-    return(
-        <div className={`pagination ${paginatorStyles.pagination}`}>
-          <span className={paginatorStyles.label}>Pages:</span>
-          {Array.from({ length: count }, (_, i) => (
-            <Link
-              activeClassName={paginatorStyles.active} 
-              className={paginatorStyles.pageLink}
-              key={`pagination-number${i + 1}`}
-              to={`/${path}/${i === 0 ? "" : i + 1}`}>
-              {i + 1}
-            </Link>
-          ))}
-        </div>
-    )
+  return(
+    <section className={`pagination ${paginatorStyles.pagination}`}>
+      <span className={paginatorStyles.label}>Pages:</span>
+      {Array.from({ length: count }, (_, i) => (
+        <Link
+          activeClassName={paginatorStyles.active} 
+          className={paginatorStyles.pageLink}
+          key={`pagination-number${i + 1}`}
+          to={`/${path}/${i === 0 ? "" : i + 1}`}>
+          {i + 1}
+        </Link>
+      ))}
+    </section>
+  )
 }
 
 export default Paginator;
