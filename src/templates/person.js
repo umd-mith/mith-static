8a956f8b07dae2167cb7501211b76bc401b6225a
kirstenkeister <kirsten.keister@gmail.com>
2020-12-07 17:25:38 -0500

    fix conflicting people css

--- src/templates/person.js
+++ src/templates/person.js
@@ -3,8 +3,8 @@ import Img from 'gatsby-image'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import './person.css'
 
+import './person.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Person = ({ pageContext: person }) => {
@@ -12,11 +12,11 @@ const Person = ({ pageContext: person }) => {
   const photo = person.headshot 
     ? <div className="headshot">
         <Img 
-        fluid={person.headshot.localFiles[0].childImageSharp.fluid} 
-        alt={`Headshot of ${person.name}`} 
-        imgStyle={{
-          objectFit: "cover",
-        }}
+          fluid={person.headshot.localFiles[0].childImageSharp.fluid} 
+          alt={`Headshot of ${person.name}`} 
+          imgStyle={{
+            objectFit: "cover",
+          }}
         />
       </div>
     : ''
@@ -51,17 +51,17 @@ const Person = ({ pageContext: person }) => {
     <Layout>
       <SEO title={name} />
       <div className="page-person">
-      <section className="person">
-        <h1 className="name">{name}</h1>
-        {photo}
-        <div className="details">
-          <h2 className="title">{person.title}</h2>
-          <div className="metadata">
-            {email} {phone} {website} {twitter}
+        <section className="person">
+          <h1 className="name">{name}</h1>
+          {photo}
+          <div className="details">
+            <h2 className="title">{person.title}</h2>
+            <div className="metadata">
+              {email} {phone} {website} {twitter}
+            </div>
           </div>
-        </div>
-        {bio}
-      </section>
+          {bio}
+        </section>
       </div>
     </Layout>
   )
