165c6802c291580628ca3353cf2ca970bd8a25bb
raffazizzi <raffaeleviglianti@gmail.com>
2021-03-25 10:43:14 -0400

    introduced a class to make it easier to get data from Airtable once

--- scripts/persist.js
+++ scripts/persist.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// This module contains some utilities for reshaping JSON from the Airtable API into 
+// This script contains the class Persistor for reshaping JSON from the Airtable API into 
 // normalized JSON objects that we can persist to the application as static data.
 
 // We recommend running this script via a package manager (e.g. npm) using `npm run persist`
@@ -11,161 +11,192 @@ const fs = require('fs')
 const path = require('path')
 const Airtable = require('airtable')
 
-const airtableKey = process.env.AIRTABLE_API_KEY
-if (! airtableKey) {
-  console.error('Please add AIRTABLE_API_KEY to your environment or .env file!')
-  process.exit()
-}
-const at = new Airtable({apiKey: airtableKey})
-
-function writeJson(o, filename) {
-  const fullPath = path.resolve(__dirname, '../static/data/', filename)
-  fs.writeFileSync(fullPath, JSON.stringify(o, null, 2) + '\n')
-  console.log(`wrote ${fullPath}`)
-}
-
-function getDataFor(base, table) {
-  return new Promise((resolve, reject) => {
-    const things = {}
-    base(table).select()
-      .eachPage(
-        async (records, nextPage) => {
-          for (const r of records) {
-            things[r.id] = r
-          }
-          nextPage()
-        },
-        (error) => {
-          if (error) {
-            console.log(`error while fetching from ${table}: ${error}`)
-            reject(error)
-          } else {
-            resolve(things)
-          }
-        }
-      )
-  })
-}
-
-async function persistPeople() {
-
-  const baseId = process.env.AIRTABLE_MITH_BASE_ID
-  if (! baseId) {
-    console.error('Please add AIRTABLE_MITH_BASE_ID to your environment or .env file!')
-    return
-  }
-  const base = at.base(baseId)
-
-  try {
-    const people = await getDataFor(base, 'People')
-    const groups = await getDataFor(base, 'Groups')
-
-    const staff = []
-
-    for (const persId in people) {
-      const person = people[persId]
-
-      const persGroups = person.get('people groups')
-      if (!persGroups) continue
-      const resolvedGroups = persGroups.map(groupId => {
-        return groups[groupId].get('type')
-      })
-
-      const persInfo = person.fields
-      persInfo['people groups'] = resolvedGroups
-      staff.push(persInfo)
+class Persistor {
+  constructor() {
+    const airtableKey = process.env.AIRTABLE_API_KEY
+    if (! airtableKey) {
+      console.error('Please add AIRTABLE_API_KEY to your environment or .env file!')
+      process.exit()
     }
-
-    writeJson(staff, 'people.json')
-  } catch(e) {
-    throw new Error(e)
+    this.at = new Airtable({apiKey: airtableKey})    
   }
-}
 
-async function persistPosts() {
-  const baseId = process.env.AIRTABLE_POSTS_BASE_ID
-  if (! baseId) {
-    console.error('Please add AIRTABLE_POSTS_BASE_ID to your environment or .env file!')
-    return
-  }
-  const base = at.base(baseId)
-  try {
-    const postsData = await getDataFor(base, 'Posts')
-
-    const posts = []
-
-    for (const postId in postsData) {
-      const post = postsData[postId]
-
-      const postInfo = post.fields
-      posts.push(postInfo)
+  get mithBase() {
+    if (this._mithBase) return this._mithBase
+    const baseId = process.env.AIRTABLE_MITH_BASE_ID
+    if (! baseId) {
+      console.error('Please add AIRTABLE_MITH_BASE_ID to your environment or .env file!')
+      process.exit()
     }
-
-    writeJson(posts, 'posts.json')
-  } catch(e) {
-    throw new Error(e)
+    this._mithBase = this.at.base(baseId)
+    return this._mithBase
   }
-}
 
-async function persistResearch() {
-  const baseId = process.env.AIRTABLE_MITH_BASE_ID
-  if (! baseId) {
-    console.error('Please add AIRTABLE_MITH_BASE_ID to your environment or .env file!')
-    return
+  get people() {
+    if (this._people) return this._people
+    this._people = this.getTable(this.mithBase, 'People')
+    return this._people
   }
-  const base = at.base(baseId)
-  try {
-    const researchItems = await getDataFor(base, 'Research')
-    const people = await getDataFor(base, 'People')
-    const identities = await getDataFor(base, 'Identities')
 
-    const research = []
+  get groups() {
+    if (this._groups) return this._groups
+    this._groups = this.getTable(this.mithBase, 'Groups')
+    return this._groups
+  }
 
-    for (const researchItemId in researchItems) {
-      const researchItem = researchItems[researchItemId]
+  get research() {
+    if (this._research) return this._research
+    this._research = this.getTable(this.mithBase, 'Research')
+    return this._research
+  }
 
-      const intParticipants = (researchItem.get('linked internal participant affiliations') || []).map(
-        idId => identities[idId].fields
-      )
+  get identities() {
+    if (this._identities) return this._identities
+    this._identities = this.getTable(this.mithBase, 'Identities')
+    return this._identities
+  }
 
-      const extParticipants = (researchItem.get('linked external participant affiliations') || []).map(
-        idId => identities[idId].fields
-      )
+  get postsBase() {
+    const baseId = process.env.AIRTABLE_POSTS_BASE_ID
+    if (! baseId) {
+      console.error('Please add AIRTABLE_POSTS_BASE_ID to your environment or .env file!')
+      process.exit()
+    }
+    return this.at.base(baseId)
+  }
 
-      for (participant of intParticipants.concat(extParticipants)) {
-        participant['linked person']
-        const person = people[participant['linked person'][0]]
-        if (!person) continue
-        participant.name = person.get('name')
+  get posts() {
+    if (this._posts) return this._posts
+    this._posts = this.getTable(this.postsBase, 'Posts')
+    return this._posts
+  }
+
+  writeJson(o, filename) {
+    const fullPath = path.resolve(__dirname, '../static/data/', filename)
+    fs.writeFileSync(fullPath, JSON.stringify(o, null, 2) + '\n')
+    console.log(`wrote ${fullPath}`)
+  }
+
+  getTable(base, table) {
+    return new Promise((resolve, reject) => {
+      const things = {}
+      base(table).select()
+        .eachPage(
+          async (records, nextPage) => {
+            for (const r of records) {
+              things[r.id] = r
+            }
+            nextPage()
+          },
+          (error) => {
+            if (error) {
+              console.log(`error while fetching from ${table}: ${error}`)
+              reject(error)
+            } else {
+              resolve(things)
+            }
+          }
+        )
+    })
+  }
+  
+  async persistPeople() {  
+    try {
+      const people = await this.people
+      const groups = await this.groups
+  
+      const staff = []
+  
+      for (const persId in people) {
+        const person = people[persId]
+  
+        const persGroups = person.get('people groups')
+        if (!persGroups) continue
+        const resolvedGroups = persGroups.map(groupId => {
+          return groups[groupId].get('type')
+        })
+  
+        const persInfo = person.fields
+        persInfo['people groups'] = resolvedGroups
+        staff.push(persInfo)
       }
-
-      researchItem.fields.participants = intParticipants
-
-      research.push(researchItem.fields)
+  
+      this.writeJson(staff, 'people.json')
+    } catch(e) {
+      throw new Error(e)
     }
-
-    writeJson(research, 'research.json')
-  } catch(e) {
-    throw new Error(e)
   }
+  
+  async persistPosts() {
+    try {
+      const postsData = await this.posts
+  
+      const posts = []
+  
+      for (const postId in postsData) {
+        const post = postsData[postId]
+  
+        const postInfo = post.fields
+        posts.push(postInfo)
+      }
+  
+      this.writeJson(posts, 'posts.json')
+    } catch(e) {
+      throw new Error(e)
+    }
+  }
+  
+  async persistResearch() {
+    try {
+      const researchItems = await this.research
+      const people = await this.people
+      const identities = await this.identities
+  
+      const research = []
+  
+      for (const researchItemId in researchItems) {
+        const researchItem = researchItems[researchItemId]
+  
+        const intParticipants = (researchItem.get('linked internal participant affiliations') || []).map(
+          id => identities[id].fields
+        )
+  
+        const extParticipants = (researchItem.get('linked external participant affiliations') || []).map(
+          id => identities[id].fields
+        )
+  
+        for (const participant of intParticipants.concat(extParticipants)) {
+          const person = people[participant['linked person'][0]]
+          if (!person) continue
+          participant.name = person.get('name')
+        }
+  
+        researchItem.fields.participants = intParticipants
+  
+        research.push(researchItem.fields)
+      }
+  
+      this.writeJson(research, 'research.json')
+    } catch(e) {
+      throw new Error(e)
+    }
+  }
+
 }
 
-async function persist() {
-  await persistPeople()
-  await persistPosts()
-  await persistResearch()
-}
-
+const persistor = new Persistor()
 switch (process.argv[2]) {
   case 'people':
-    persistPeople()
+    persistor.persistPeople()
     break
   case 'posts':
-    persistPosts()
+    persistor.persistPosts()
     break
   case 'research':
-    persistResearch()
+    persistor.persistResearch()
     break
   default:
-    persist()
+    persistor.persistPeople()
+    persistor.persistPosts()
+    persistor.persistResearch()
 }
