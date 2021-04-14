8cca441d411a195ccfc94ec37bea67cc46b05143
raffazizzi <raffaeleviglianti@gmail.com>
2021-04-14 14:48:33 -0400

    fixed after merge

--- src/templates/event.js
+++ src/templates/event.js
@@ -12,8 +12,8 @@ import './event.css'
 
 const Event = ({ pageContext: item }) => {
   const title = item.image
-  ? <Img 
-    fluid={item.image.localFiles[0].childImageSharp.fluid} 
+  ? <GatsbyImage 
+    fluid={item.image.localFiles[0].childImageSharp.gatsbyImageData} 
     alt={item.event_title} 
     className="event-image" 
   /> : <h1 className="title" itemProp="name">{item.talk_title || item.event_title}</h1>
--- src/templates/research.js
+++ src/templates/research.js
@@ -1,5 +1,5 @@
 import React from 'react'
-//import { Link } from 'gatsby'
+import { Link } from 'gatsby'
 import { GatsbyImage } from 'gatsby-plugin-image'
 
 import Layout from '../components/layout'
