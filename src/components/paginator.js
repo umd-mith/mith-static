9aa3f3490b126e18ecfb260461ff70158f21f9e9
raffazizzi <raffaeleviglianti@gmail.com>
2021-04-14 14:35:36 -0400

    importing css module properly

--- src/components/paginator.js
+++ src/components/paginator.js
@@ -1,16 +1,16 @@
 import { Link } from 'gatsby'
 import React from 'react'
 
-import './paginator.module.css'
+import * as paginatorStyles from './paginator.module.css'
 
 const Paginator = ({count, path}) => {
   return(
-    <section className="pagination">
-      <span className="label">Pages:</span>
+    <section className={`pagination ${paginatorStyles.pagination}`}>
+      <span className={paginatorStyles.label}>Pages:</span>
       {Array.from({ length: count }, (_, i) => (
         <Link
-          activeClassName="active" 
-          className="pageLink"
+          activeClassName={paginatorStyles.active} 
+          className={paginatorStyles.pageLink}
           key={`pagination-number${i + 1}`}
           to={`/${path}/${i === 0 ? "" : i + 1}`}>
           {i + 1}
