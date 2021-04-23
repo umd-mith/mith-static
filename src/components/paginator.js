760f88509f4b82f2bc12f770e9cb7ffc2112a239
kirstenkeister <kirsten.keister@gmail.com>
2021-04-23 00:35:53 -0400

    fix pagination module layout and styles

--- src/components/paginator.js
+++ src/components/paginator.js
@@ -1,21 +1,24 @@
 import { Link } from 'gatsby'
 import React from 'react'
 
-import * as paginatorStyles from './paginator.module.css'
+import './paginator.css'
 
 const Paginator = ({count, path}) => {
   return(
-    <section className={`pagination ${paginatorStyles.pagination}`}>
-      <span className={paginatorStyles.label}>Pages:</span>
-      {Array.from({ length: count }, (_, i) => (
-        <Link
-          activeClassName={paginatorStyles.active} 
-          className={paginatorStyles.pageLink}
-          key={`pagination-number${i + 1}`}
-          to={`/${path}/${i === 0 ? "" : i + 1}`}>
-          {i + 1}
-        </Link>
-      ))}
+    <section className="pagination-wrap">
+      <ul className="pagination">
+        {Array.from({ length: count }, (_, i) => (
+          <li className="page-link">
+            <Link
+              activeClassName="active" 
+              className="page-link"
+              key={`pagination-number${i + 1}`}
+              to={`/${path}/${i === 0 ? "" : i + 1}`}>
+              {i + 1}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </section>
   )
 }
