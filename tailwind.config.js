795ae70f4dede05f6db33c6ae6f444226632a1e8
Raff Viglianti <raffaeleviglianti@gmail.com>
2022-07-20 16:50:46 -0400

    started moving global styles to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,19 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,}'],
+  plugins: [require('@tailwindcss/typography')],
+  theme: {
+    colors: {
+      mithblue: '#005296',
+      mithgray: '#43454B',
+      mithgreen: '#AEBB20',
+      mithyellow: '#FEE63E',
+      umdyellow: '#FCD116',
+      umdred: '#CE1126',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+    },
+  },
 };
