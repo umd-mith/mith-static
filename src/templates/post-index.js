a1d52facfa1e9e1ea6ca6c652f5266b4ce887cea
Trevor Muñoz <trevor.munoz@gmail.com>
2024-07-18 17:54:24 -0400

    Make failing test re: news page h1 pass

--- src/templates/post-index.js
+++ src/templates/post-index.js
@@ -1,56 +1,57 @@
-import React from 'react'
-import { graphql, Link } from 'gatsby'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from "react"
+import { graphql, Link } from "gatsby"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-import Layout from '../components/layout'
-import Paginator from '../components/paginator'
-import SEO from '../components/seo'
+import Layout from "../components/layout"
+import Paginator from "../components/paginator"
+import SEO from "../components/seo"
 
-import './post-index.css'
+import "./post-index.css"
 
-const PostIndex = ({data}) => {
+const PostIndex = ({ data }) => {
   const posts = data.allAirtablePosts.nodes
   const pageCount = data.allAirtablePosts.pageInfo.pageCount
 
   return (
-		<Layout>
+    <Layout>
       <SEO title="MITH News" />
       <div className="page-news">
         <section className="posts news">
-          <h1 className="page-title">
-            News &nbsp;
+          <div className="page-title">
             <a href="/news/feed.xml" className="icon-rss">
               <FontAwesomeIcon title="News RSS Feed" icon="rss" />
             </a>
-          </h1>
+            <h1>News</h1>
+          </div>
+
           {posts.map(_post => {
             const post = _post.data
-            const slug = '/news/' + post.slug
-            const markdownFile = post.slug + '.md'
+            const slug = "/news/" + post.slug
+            const markdownFile = post.slug + ".md"
 
             // pick out the markdown file that has the same slug
-            const doc = data.allFile.nodes.find(
-              n => n.childMarkdownRemark.fileAbsolutePath.match(markdownFile)
+            const doc = data.allFile.nodes.find(n =>
+              n.childMarkdownRemark.fileAbsolutePath.match(markdownFile),
             )
 
             // if there is no doc then we're missing the markdown file for a blog
-            // post that is in airtable 
-            
-            if (! doc) {
+            // post that is in airtable
+
+            if (!doc) {
               console.warn(`missing markdown post for slug ${post.slug}`)
               // throw new Error(`missing markdown post for slug ${post.slug}`)
-              return <article className="post news-post" key={`news-${post.id}`}>
-                <h2 className="post-title">
-                  <Link>{post.post_title}</Link>
-                </h2>
-                <div className="meta">
-                  by <span className="author">{post.author_name}</span>
-                  {' '}on <time>{post.post_date}</time>
-                </div>
-                <div className="excerpt">
-                  Currently unavailable.
-                </div>
-              </article>
+              return (
+                <article className="post news-post" key={`news-${post.id}`}>
+                  <h2 className="post-title">
+                    <Link>{post.post_title}</Link>
+                  </h2>
+                  <div className="meta">
+                    by <span className="author">{post.author_name}</span> on{" "}
+                    <time>{post.post_date}</time>
+                  </div>
+                  <div className="excerpt">Currently unavailable.</div>
+                </article>
+              )
             }
 
             return (
@@ -59,12 +60,14 @@ const PostIndex = ({data}) => {
                   <Link to={slug}>{post.post_title}</Link>
                 </h2>
                 <div className="meta">
-                  by <span className="author">{post.author_name}</span>
-                  {' '}on <time>{post.post_date}</time>
+                  by <span className="author">{post.author_name}</span> on{" "}
+                  <time>{post.post_date}</time>
                 </div>
                 <div className="excerpt">
-                  {doc.excerpt} 
-                  <Link to={slug} className="read-more">continue reading</Link>
+                  {doc.excerpt}
+                  <Link to={slug} className="read-more">
+                    continue reading
+                  </Link>
                 </div>
               </article>
             )
@@ -81,7 +84,7 @@ export const query = graphql`
     allAirtablePosts(
       limit: $limit
       skip: $skip
-      sort: {data: {post_date: DESC}}
+      sort: { data: { post_date: DESC } }
     ) {
       nodes {
         data {
@@ -96,7 +99,7 @@ export const query = graphql`
         pageCount
       }
     }
-    allFile(filter: {sourceInstanceName: {eq: "news"}}) {
+    allFile(filter: { sourceInstanceName: { eq: "news" } }) {
       nodes {
         childMarkdownRemark {
           excerpt(pruneLength: 250)
@@ -107,5 +110,5 @@ export const query = graphql`
     }
   }
 `
- 
+
 export default PostIndex
