43da0392e7f744f9a664f4f8185609fd173fcb7d
Trevor Muñoz <trevor.munoz@gmail.com>
2025-03-03 16:36:04 -0500

    Add tailwindcss and svelte integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from "astro/config";
 
+import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
